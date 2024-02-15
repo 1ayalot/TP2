e206dc53d72748d713350df148febc8a2235c531
ayalo <ayalo@Aya>
2024-02-15 16:24:40 +0100

    tp2

---
+++ Livre/src/Livre.java
@@ -0,0 +1,22 @@
+
+public class Livre {
+	
+	//Attribut
+	private String titre;
+	private String auteur;
+	private int anneePublication;
+	 
+	// Constructeur
+	public Livre(String titre, String auteur, int anneePublication) {
+		this.titre = titre;
+		this.auteur = auteur;
+	    this.anneePublication = anneePublication;
+	 }
+	 
+	 // Méthode afficherDetails
+	 public void afficherDetails() {
+		 System.out.println("Le titre : " + titre);
+	     System.out.println("L'auteur : " + auteur);
+	     System.out.println("L'année de publication : " + anneePublication);
+	 }
+}
